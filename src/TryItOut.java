9a0624f9037b69a4515259a02b99ba16236af8f0
John Bowser <john.bow7720@gmail.com>
2020-11-19 14:12:36 -0700

    Practice with variables

--- src/TryItOut.java
+++ src/TryItOut.java
@@ -1,5 +1,22 @@
 public class TryItOut {
     public static void main(String[] args) {
         System.out.println("Try it out class");
+
+        //Declare some variables
+        int cupsOfCoffeeMonday;
+        int cupsOfCoffeeTuesday;
+        int cupsOfCoffeeWednesday;
+        int cupsOfCoffeeThursday;
+        int cupsOfCoffeeFriday;
+
+        //Assign some values
+        cupsOfCoffeeMonday = 4;
+        cupsOfCoffeeTuesday = 3;
+        cupsOfCoffeeWednesday = 6;
+        cupsOfCoffeeThursday = 5;
+        cupsOfCoffeeFriday = 4;
+
+        //print a variable
+        System.out.println(cupsOfCoffeeFriday);
     }
 }
