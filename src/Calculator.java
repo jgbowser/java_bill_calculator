dd45aba6a2a2c8e33d10097eeee87e683e15ca11
John Bowser <john.bow7720@gmail.com>
2020-11-28 09:55:04 -0700

    Added necessary info to Calculator class using data types that were covered in lesson

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,11 +9,48 @@ public class Calculator {
         double personOneAppetizerCost = 9.99;
         double personOneMainCost = 23.99;
         double personOneDessertCost = 10.29;
-        double personOneDrinkCost = 4.25;
+        double personOneDrinkCost = 8.50;
 
         double personTwoAppetizerCost = 12.99;
         double personTwoMainCost = 18.99;
         double personTwoDessertCost = 9.99;
         double personTwoDrinkCost = 4.25;
+
+        double personThreeAppetizerCost = 10.49;
+        double personThreeMainCost = 26.79;
+        double personThreeDessertCost = 10.29;
+        double personThreeDrinkCost = 3.75;
+
+        double personFourAppetizerCost = 13.79;
+        double personFourMainCost = 25.99;
+        double personFourDessertCost = 4.49;
+        double personFourDrinkCost = 7.50;
+
+        String personOneName = "Adeline";
+        String personOneAppetizer = "Stuffed Ziti Fritta";
+        String personOneMain = "Shrimp Scampi";
+        String personOneDessert = "Sicilian Cheesecake";
+        String personOneDrink = "Raspberry Lemonade x 2";
+
+        String personTwoName = "Brixton";
+        String personTwoAppetizer = "Lasagna Fritta";
+        String personTwoMain = "Fettuccine Alfredo";
+        String personTwoDessert = "Lemon Cream cake";
+        String personTwoDrink = "Mango-Strawberry Iced Tea";
+
+        String personThreeName = "Cora";
+        String personThreeAppetizer = "Fried Mozzarella";
+        String personThreeMain = "Tour of Italy";
+        String personThreeDessert = "Zeppoli";
+        String personThreeDrink = "Fresh Brewed Iced Tea";
+
+        String personFourName = "Dean";
+        String personFourAppetizer = "Classic Shrimp Scampi Fritta";
+        String personFourMain = "Seafood Alfredo";
+        String personFourDessert = "Dolcini";
+        String personFourDrink = "Coke x 2";
+
+        boolean serviceWasGood = true;
+        boolean splitBillEvenly = true;
     }
 }
